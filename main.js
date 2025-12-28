dca5c146f869bf89bb89eeb2da1b66c385c8fca6
daannjciee9201 <1113211024@gap.wzu.edu.tw>
2025-12-28 12:53:59 +0800

    1228

---
+++ main.js
@@ -0,0 +1,9 @@
+import './style.scss'  // 這行沒問題
+import 'bootstrap/dist/css/bootstrap.min.css' // 加入這行確保基礎樣式必載入
+import 'bootstrap'
+
+console.log('Hiphop mode ON!');
+
+document.addEventListener('DOMContentLoaded', () => {
+    // 這裡可以寫之後的互動功能
+});
